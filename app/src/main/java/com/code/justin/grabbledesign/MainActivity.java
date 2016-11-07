1ee35a348043fa6277a86f19cff7e7b36841bdf9
ferame <alisauskas.j@gmail.com>
2016-11-07 20:46:51 +0000

    Initial commit

---
+++ app/src/main/java/com/code/justin/grabbledesign/MainActivity.java
@@ -0,0 +1,21 @@
+package com.code.justin.grabbledesign;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toMapsActivity(View view){
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent);
+    }
+
+}
