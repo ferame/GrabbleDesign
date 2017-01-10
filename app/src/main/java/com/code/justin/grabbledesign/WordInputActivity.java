89df56094ac6ac43b4c269d4ccb65b7c2c3bfd15
ferame <alisauskas.j@gmail.com>
2017-01-10 21:48:12 +0200

    Added button colouring and count index

--- app/src/main/java/com/code/justin/grabbledesign/WordInputActivity.java
+++ app/src/main/java/com/code/justin/grabbledesign/WordInputActivity.java
@@ -3,18 +3,29 @@ package com.code.justin.grabbledesign;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.SuperscriptSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.phenotype.Flag;
 
 public class WordInputActivity extends AppCompatActivity implements View.OnClickListener{
 
     private int Player;
 
     private Letter[] lettersParam = new Letter[26];
+
+    Button[] buttons = new Button[28];
 
     class Letter{
         int count;
@@ -45,11 +56,12 @@ public class WordInputActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_word_input);
         Intent intent = getIntent();
         Player = Integer.parseInt(intent.getStringExtra("userId"));
+
         Log.i("Populate", "Starts");
         populateLettersParam();
         Log.i("Populate", "Ends");
 
-        Button[] buttons = new Button[28];
+//        Button[] buttons = new Button[28];
         Integer i = 0;
         for(char alphabet = 'A'; alphabet <= 'Z';alphabet++) {
             String buttonID = alphabet + "letter";
@@ -59,6 +71,14 @@ public class WordInputActivity extends AppCompatActivity implements View.OnClick
             buttons[i].setOnClickListener(this);
             i++;
         }
+
+        Log.i("Button Count info", "Starts");
+        setLetterButtonCounts();
+        Log.i("Button Count info", "Ends");
+
+        Log.i("Button Colouring", "Starts");
+        setLetterButtonColours();
+        Log.i("Button Colouring", "Ends");
 //        for (int i = 0; i < buttons.length; i++) {
 //            {
 //                String buttonID = "sound" + (i + 1);
@@ -236,9 +256,11 @@ public class WordInputActivity extends AppCompatActivity implements View.OnClick
         c.moveToFirst();
         Integer count = 0;
         while (c != null){
+            Log.i("getting letter", "ok?");
             Character letter = c.getString(letterIndex).charAt(0);
+            Log.i("Found letter", letter.toString());
             Integer amount = Integer.parseInt(c.getString(amountIndex));
-
+            lettersParam[count] = new Letter();
             lettersParam[count].setLetter(letter);
             lettersParam[count].setCount(amount);
 
@@ -252,5 +274,33 @@ public class WordInputActivity extends AppCompatActivity implements View.OnClick
         }
         c.close();
         userData.close();
+    }
+
+    private void setLetterButtonCounts(){
+        for (int i = 0; i < 26; i++){
+            Character letter = lettersParam[i].getLetter();
+            Integer count = lettersParam[i].getCount();
+
+            String toShow = letter + count.toString();
+            SpannableStringBuilder showSpan = new SpannableStringBuilder(toShow);
+            showSpan.setSpan(new SuperscriptSpan(), 1, showSpan.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            showSpan.setSpan(new RelativeSizeSpan(0.7f), 1, showSpan.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            buttons[i].setText(showSpan);
+        }
+    }
+
+    private void setLetterButtonColours(){
+        for (int i = 0; i < 26; i++){
+            Integer count = lettersParam[i].getCount();
+
+            if (count > 3){
+                buttons[i].setBackgroundResource(R.drawable.keyboard_button_border);
+            } else if ( count > 0){
+                buttons[i].setBackgroundResource(R.drawable.keyboard_button_border_low);
+            } else {
+                buttons[i].setBackgroundResource(R.drawable.keyboard_button_border_disabled);
+            }
+        }
     }
 }
